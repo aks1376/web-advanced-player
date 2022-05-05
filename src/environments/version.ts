d8837ef0c470c6b5701356447d4c613ca5d67c9d
Ali Karimi <alikarimi.s1376@gmail.com>
2022-05-05 22:28:57 +0430

    added use git commit hash as version to update clients for new update

---
+++ src/environments/version.ts
@@ -0,0 +1,15 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+/* eslint-disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "db8c502-dirty",
+    "hash": "db8c502",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "db8c502-dirty",
+    "semverString": null,
+    "version": "1.1.0"
+};
+/* tslint:enable */
